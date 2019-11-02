5641495cb197c013e91d357d634d75ff663e016f
mostafa-mohamed255 <57295734+mostafa-mohamed255@users.noreply.github.com>
2019-11-03 00:49:54 +0200

    progict

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$('.counter').counterUp();var options = {
+    strings: ['Web Designer', 'CEO DevFolio', 'Web Developer', 'Frontend Developer'],
+
+    loop: true,
+    loopCount: Infinity,
+    typeSpeed: 170
+};
+var typed = new Typed('.typed', options);
